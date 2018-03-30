5d9d350800e4a7ee6f4e3f0db2a20a77207a1278
jacobsa20 <jacobsa20@up.edu>
2018-03-29 19:03:42 -0700

    I tried so hard, and got so far, but in the end it doesn't even matter

--- PongGame.java
+++ PongGame.java
@@ -138,7 +138,6 @@ public class PongGame implements Animator {
     public void onTouch(MotionEvent event) {
         //ability to move paddle
         int pos=event.getActionMasked();
-       // midPaddleAI=(int) event.getY();
         switch(pos) {
             case MotionEvent.ACTION_DOWN:
                 midPaddle = (int) event.getY();
